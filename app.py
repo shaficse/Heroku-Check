d87dbd6097c6811e3d7bac7a2dafe31b72e4bd67
Shafi Sourov <sourov.cse.brur@gmail.com>
2023-10-27 12:14:43 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import numpy as np 
 from flask import Flask, request, jsonify, render_template 
-import pickle 
+# import pickle 
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl','rb'))
+# model = pickle.load(open('model.pkl','rb'))
 
 @app.route('/')
 def home():
@@ -16,9 +16,10 @@ def predict():
     """
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-    output = round(prediction[0], 2)
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    # prediction = model.predict(final_features)
+    # output = round(prediction[0], 2)
+    # return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='Employee Salary should be $546454')
 
 if __name__ == "__main__":
     app.run(debug=True)
