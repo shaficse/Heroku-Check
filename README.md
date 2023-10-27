f350c9d83260cc862601f9716e10b50a0f01d526
Shafi Sourov <sourov.cse.brur@gmail.com>
2023-10-27 11:58:09 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Heroku-Demo
+### Deploying a Machine learning model in Heroku (PAAS)
+
+#### App two 
+
+This is second app of Build Train Deploy 10 AI apps on cloud project
+
+https://github.com/ajithvallabai/Build_Train_Deploy_10_AI_apps
+
+
+App link:
+
+- https://salarayprediction.herokuapp.com/
+
+**Use case**
+
+- Trained a basic regressor and deployed on heroku
+
+
+
+#### Credits
+
+- [Krishna -Youtuber](https://github.com/krishnaik06)
--- model.py
+++ model.py
@@ -1,39 +1,60 @@
-# Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+import numpy as np 
+import pandas as pd 
 import pickle
 
-dataset = pd.read_csv('hiring.csv')
-
-dataset['experience'].fillna(0, inplace=True)
-
-dataset['test_score'].fillna(dataset['test_score'].mean(), inplace=True)
-
-X = dataset.iloc[:, :3]
-
-#Converting words to integer values
 def convert_to_int(word):
-    word_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8,
-                'nine':9, 'ten':10, 'eleven':11, 'twelve':12, 'zero':0, 0: 0}
+    word_dict = {
+        'one':1,
+        'two':2,
+        'three':3,
+        'four':4,
+        'five':5,
+        'six':6,
+        'seven':7,
+        'eight':8,
+        'nine':9,
+        'ten':10,
+        'eleven':11,
+        'twelve':12,
+        'zero':0,
+        0:0
+    }
     return word_dict[word]
 
-X['experience'] = X['experience'].apply(lambda x : convert_to_int(x))
+if __name__ ==  "__main__":
 
-y = dataset.iloc[:, -1]
+    dataset = pd.read_csv('hiring.csv')
+    #print(dataset.head())
 
-#Splitting Training and Test Set
-#Since we have a very small dataset, we will train our model with all availabe data.
+    # Data cleaning
+    dataset['Experience'].fillna(0, inplace=True)
+    dataset['test_score'].fillna(dataset['test_score'].mean(), inplace=True)
 
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
+    # Train data
+    X = dataset.iloc[:, :3]
+    # print(X)
+    # if True:
+    #     exit()
 
-#Fitting model with trainig data
-regressor.fit(X, y)
+    X['Experience'] = X['Experience'].apply(lambda x: convert_to_int(x))
 
-# Saving model to disk
-pickle.dump(regressor, open('model.pkl','wb'))
+    y = dataset.iloc[:, -1]
+    
 
-# Loading model to compare the results
-model = pickle.load(open('model.pkl','rb'))
-print(model.predict([[2, 9, 6]]))
+    # Training
+
+    from sklearn.linear_model import LinearRegression
+    regressor = LinearRegression()
+    
+    regressor.fit(X,y)
+
+    pickle.dump(regressor, open('model.pkl', 'wb'))
+
+    model = pickle.load(open('model.pkl','rb'))
+    print(model.predict([[1,7,7]]))
+
+
+
+
+
+
